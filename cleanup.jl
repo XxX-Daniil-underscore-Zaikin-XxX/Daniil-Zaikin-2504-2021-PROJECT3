560586689d75a16245b63043dc2531bd71ef4d67
Dan Z <dan.zaikin@gmail.com>
2021-11-18 13:13:24 +1000

    cleans up code, adds proper docs

--- cleanup.jl
+++ cleanup.jl
@@ -3,10 +3,15 @@ using CSV
 
 include("coordinates.jl")
 
+# All relevant filenames
 LESS_FILE = "MELBOURNE_HOUSE_PRICES_LESS.csv"
 FULL_FILE = "Melbourne_housing_FULL.csv"
 FULL_FILLED_FILE_PROCESSED = "Melbourne_housing_FULL_processed.csv"
 
+# Set to true if current processed files need to be overwritten or don't exist
+OVERWRITE_PROCESSED = false
+
+# Gonna put this in a .env later - if this was a real repo, I'd do it earlier
 API_KEY = "AIzaSyAnxxxFGwpJKRaGGAv0De9hNL0OqlHT6B0"
 
 """
@@ -32,21 +37,15 @@ uppercasewordfirst(name::AbstractString)::String = join(uppercasefirst.(lowercas
 """
     consolidate_string_case(df::AbstractDataFrame, cols=[:Suburb, :Address, :Regionname, :CouncilArea])::DataFrame
 
-Returns a copy of the given `DataFrame`, where the data on each 
+Returns a copy of the given `DataFrame`, where the (`String``) data in each of the given columns is changed as per the `uppercasewordfirst` function.
 """
 consolidate_string_case(df::AbstractDataFrame, cols=[:Suburb, :Address, :Regionname, :CouncilArea])::DataFrame = transform(df, [col => ByRow(x -> uppercasewordfirst(x)) => col for col in cols]...)
 
-df_init = DataFrame(CSV.File(LESS_FILE))
+df_less = DataFrame(CSV.File(LESS_FILE)) |> consolidate_string_case
 
-# df = transform(df_init, [:Suburb, :Address, :Regionname, :CouncilArea] => ByRow((a, b, c, d) -> uppercasewordfirst.(a, b, c, d)) => [:Suburb, :Address, :Regionname, :CouncilArea])
+df_full = DataFrame(CSV.File(FULL_FILE)) |> consolidate_string_case |> fix_coordinate_typos
 
-df = consolidate_string_case(df_init)
-
-# show(df_init)
-# show(df)
 x = combine(groupby(df, [:Suburb]), nrow => :count)
-
-# x[x.count .= 1, :]
 
 y = filter(row -> row.count == 1, x)
 
@@ -57,74 +56,80 @@ y_1 = filter(row -> row.count == 1, x_1)
 
 @show(y_1)
 
-# z_cases = .!completecases(df)
+"""
+    find_missing_subset(dataframe::AbstractDataFrame, columns::Symbol=:)::DataFrame
 
-# z = df[z_cases, :]
+Returns a copy of the given `DataFrame` that has `missing` records in any of the passed columns (all columns by default)
+"""
+find_missing_subset(dataframe::AbstractDataFrame, columns::Symbol=:)::DataFrame = dataframe[.!completecases(dataframe, columns), :]
 
-# @show(z)
+"""
+    list_missing_data(dataframe::AbstractDataFrame, columns::Symbol=:)
 
-find_missing_subset(dataframe, columns=:) = dataframe[.!completecases(dataframe, columns), :]
-
-
-
-function list_missing_data(dataframe, columns=:)
+If `JULIA_DEBUG`, prints some information about missing data in the given columns. Otherwise, it does nothing.
+"""
+function list_missing_data(dataframe::AbstractDataFrame, columns::Symbol=:)
     for col in propertynames(dataframe[!, columns])
         n_rows = nrow(find_missing_subset(dataframe, col))
         if n_rows > 0
-            print(col, " has ", n_rows, " missing rows.\n")
+            @debug "$(col) has $(n_rows) missing rows."
         end
     end
 end
 
-print("\nPrinting LESS data:\n\n")
-list_missing_data(df)
+@debug "Missing LESS data:" list_missing_data(df_less)
+
+@debug "Missing FULL data:" list_missing_data(df_full)
+
+@debug "Missing latitudes/longitudes:" find_missing_subset(df_full, [:Latitude, :Longitude])
 
 
+"""
+    get_block_of_missing_coords(df::AbstractDataFrame, to_edit::Bool, block_size=100)::DataFrame
 
-df_full = fix_coordinate_typos(transform(DataFrame(CSV.File(FULL_FILE)), :Suburb => x -> uppercasefirst.(x)))
-
-# Price cannot be imputed, must be dropped for tasks
-
-# Lati
-
-print("\nPrinting FULL data:\n\n")
-list_missing_data(df_full)
-
-print("\nPrinting missing latitudes/longitudes:\n\n")
-#print(find_missing_subset(df_full, [:Latitude, :Longitude]))
-
-
-
-function get_block_of_missing_coords(df::DataFrame, to_edit::Bool, block_size=100)
+Specialised function. Returns a `DataFrame` that contains the last `block_size` rows of `df` that are missing their `Latitude` and `Longitude` if `to_edit` is `true`; otherwise, returns a `DataFrame` that contains all records with missing coordinates save for the last `block_size` rows.
+"""
+function get_block_of_missing_coords(df::AbstractDataFrame, to_edit::Bool, block_size=100)::DataFrame
     missing_df = df[.!completecases(df, [:Latitude, :Longitude]), :]
     return missing_df[vcat(max(nrow(missing_df) - block_size, 0) |> trues, min(block_size, nrow(missing_df)) |> falses) .⊻ to_edit, :]
 end
 
-# CSV.write(FULL_FILLED_FILE, df_full)
+OVERWRITE_PROCESSED && CSV.write(FULL_FILLED_FILE, df_full)
 
+# Loop through our missing coordinates and fill them in chunk-by-chunk. Writes to the file and reads from it once each loop.
 while true
+    # Read a fresh DataFrame from the file (slow, but saves on API calls in case of error)
     df_filled = DataFrame(CSV.File(FULL_FILLED_FILE_PROCESSED))
 
+    # Populate the missing set we won't touch, and the one we will
     missing_coords_yesedit = get_block_of_missing_coords(df_filled, true)
     missing_coords_noedit = get_block_of_missing_coords(df_filled, false)
 
-    print("Editing ", nrow(missing_coords_yesedit), " rows.\n")
-    print("Leaving behind ", nrow(missing_coords_noedit), " rows.\n")
+    @info "Editing ", nrow(missing_coords_yesedit), " rows."
+    @info "Leaving behind ", nrow(missing_coords_noedit), " rows."
 
-    nrow(missing_coords_yesedit) == 0 && break
+    if nrow(missing_coords_yesedit) == 0 
+        @info "Finished filling coordinates."
+        break
+    end
 
+    # Populate the missing coordinates
     transform!(missing_coords_yesedit, [:Suburb, :Address] => ByRow((subr, addr) -> get_coordinates(addr, subr, API_KEY)) => [:Latitude, :Longitude])
-
-    # @show missing_coords
 
     len_filled = nrow(df_filled)
 
+    # Re-arrange and fill our file-read DataFrame with our new data
     filled_df = append!(append!(dropmissing(df_filled, [:Latitude, :Longitude]) |> allowmissing!,  missing_coords_yesedit), missing_coords_noedit)
 
     row_diff = len_filled - nrow(filled_df)
 
-    print("Difference in rows: ", row_diff, ".\n")
-    row_diff != 0 && break
+    @info "Difference in rows: ", row_diff, ".\n"
+    if row_diff != 0
+        # Likely that something's gone wrong, will need a human eye
+        @error "Output `DataFrame` differs from input `DataFrame`. Exiting."
+        break
+    end
 
+    # Overwrite the file
     CSV.write(FULL_FILLED_FILE_PROCESSED, filled_df)
 end
--- coordinates.jl
+++ coordinates.jl
@@ -2,28 +2,41 @@ using HTTP
 using URIs
 using JSON
 
-GEOCODE_SCHEME = "https"
-GEOCODE_HOST = "maps.googleapis.com"
-GEOCODE_PATH = "/maps/api/geocode/json"
-GEOCODE_ADDRESS_KEY = "address"
-GEOCODE_API_KEY = "key"
-GEOCODE_ADDRESS_SUFFIX = "Victoria Australia"
+# Some basic constants to do URI
+const GEOCODE_SCHEME = "https"
+const GEOCODE_HOST = "maps.googleapis.com"
+const GEOCODE_PATH = "/maps/api/geocode/json"
+const GEOCODE_ADDRESS_KEY = "address"
+const GEOCODE_API_KEY = "key"
+const GEOCODE_ADDRESS_SUFFIX = "Victoria Australia"
 
 """
-Construct a URI using the library and our constants
+    generate_geocode_uri(address::String, suburb::String, api_key::String)::URI
+
+Construct `URI` based on our constants and given variables
 """
-generate_geocode_uri(address, suburb, api_key) =
+generate_geocode_uri(address::String, suburb::String, api_key::String)::URI =
     URI(scheme=GEOCODE_SCHEME, host=GEOCODE_HOST, path=GEOCODE_PATH, query=Dict([(GEOCODE_ADDRESS_KEY, string(address, " ", suburb, " ", GEOCODE_ADDRESS_SUFFIX)), (GEOCODE_API_KEY, api_key)]))
 
 """
-Send a request through the URI
+    get_geocode(address, suburb, api_key)
+
+Send a GET request to Google's Geocoding API based on the given vars and our constants
 """
 get_geocode(address, suburb, api_key) = HTTP.get(generate_geocode_uri(address, suburb, api_key))
 
 """
-Format the output
+    get_coordinates(address::String, suburb::String, api_key::String)::Tuple{Float64, Float64}
+
+Takes a street address and a suburb, and returns a `Tuple` (Latitude, Longitude) of that house's coordinates - as per Google's Geocoding API. It assumes the house is in Victoria, Australia.
+
+# Examples
+```jldoctest
+julia>get_coordinates("85 studley st", "abbotsford", "ImaginaryAPIKey")
+(-37.8015848, 144.99679)
+```
 """
-function get_coordinates(address, suburb, api_key)
+function get_coordinates(address::String, suburb::String, api_key::String)::Tuple{Float64, Float64}
     location = get_geocode(address, suburb, api_key).body |> String |> JSON.parse |> x->x["results"][1]["geometry"]["location"]
     return location["lat"], location["lng"]
 end
